75b3c66684e6d4247d8badfbbdac99f0b2c22040
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2023-03-13 21:28:03 -0300

    Finalizando página de histórico

---
+++ src/pages/history/index.tsx
@@ -0,0 +1,55 @@
+import { HistoryTableWrapper, Status, Wrapper } from './styles'
+
+export const History = () => {
+  return (
+    <Wrapper>
+      <h1>Meu histórico</h1>
+      <HistoryTableWrapper>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>25 minutos</td>
+              <td>Há 1 mês</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>25 minutos</td>
+              <td>Há 1 mês</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>25 minutos</td>
+              <td>Há 1 mês</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>25 minutos</td>
+              <td>Há 1 mês</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryTableWrapper>
+    </Wrapper>
+  )
+}
